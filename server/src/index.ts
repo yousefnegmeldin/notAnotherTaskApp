0888ad4bec5049ab35169753462a75c03c1e0751
yousefnegm <yousefnegm@gmail.com>
2024-02-13 15:10:09 +0200

    Update CORS configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ app.use(express.json());
 const corsOptions = {
   origin: "https://not-another-task-app.vercel.app",
 };
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.delete("/decks/:id", deleteDeckController);
 app.get("/decks", getDecksController);
