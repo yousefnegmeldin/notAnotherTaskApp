929fbffc3b6b6e189a421692ffb123268773526c
yousefnegm <yousefnegm@gmail.com>
2024-02-13 15:26:16 +0200

    Refactor CORS configuration in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,26 +17,19 @@ const mongo_uri = process.env.MONGO_URI;
 
 const app = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://not-another-task-app.vercel.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Private-Network", "true");
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
+app.use(
+  cors({
+    origin: "https://not-another-task-app.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these HTTP methods
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Origin",
+      "X-Requested-With",
+      "Accept",
+    ], // Allow these headers
+  })
+);
 
 app.delete("/decks/:id", deleteDeckController);
 app.get("/decks", getDecksController);
