0c2681f8a245fa4eda9d1a380d4a269a429114c8
yousefnegm <yousefnegm@gmail.com>
2024-02-13 15:39:22 +0200

    last one please

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,23 @@ const mongo_uri = process.env.MONGO_URI;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://not-another-task-app.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, X-Api-Key"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  if ("OPTIONS" === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.delete("/decks/:id", deleteDeckController);
 app.get("/decks", getDecksController);
