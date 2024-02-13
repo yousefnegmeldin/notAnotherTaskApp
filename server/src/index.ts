bb3ac8abcbab7bdf898405cc150d1f885a9507c9
yousefnegm <yousefnegm@gmail.com>
2024-02-13 15:14:00 +0200

    Add CORS headers to allow cross-origin requests

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,6 +22,27 @@ const corsOptions = {
 };
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://not-another-task-app.vercel.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Private-Network", "true");
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 app.delete("/decks/:id", deleteDeckController);
 app.get("/decks", getDecksController);
 app.post("/decks", createDeckController);
